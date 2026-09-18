206b42afd82bac1069b8f0e0001258801c77f147
Mahesh Raut <rautmahesh300@gmail.com>
2026-09-18 13:17:12 +0530

    lastest

--- Fixtures/login.js
+++ Fixtures/login.js
@@ -1,12 +1,3 @@
-import { test as base, expect } from '@playwright/test'
+import {test as base, expect} from '@playwright/test'
 
-export const test = base.extend({
-    driver: async ({ page }, use) => {
-        await page.goto("https://rahulshettyacademy.com/client/#/auth/login");
-        await page.fill("#userEmail", "rautmahesh300@gmail.com");
-        await page.fill("#userPassword", "HPpavillion59K#");
-        await page.click('[id="login"]');
-        await use(page);
-    }
-})
-export {expect}
+export const test 
